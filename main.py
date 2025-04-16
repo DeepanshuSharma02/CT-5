c451650a0330a48d4a1ff05b1ad4518e72f502c9
Deepanshu Sharma <dsharma02102004@gmail.com>
2025-04-16 15:52:01 +0530

    hello4

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import joblib
+from fastapi.middleware.cors import CORSMiddleware
 
 # Load model
 model = joblib.load("legal_classifier.pkl")
 
 # FastAPI app
 app = FastAPI()
+
+# CORS middleware setup to allow all origins and methods
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
 
 class TextRequest(BaseModel):
     text: str
